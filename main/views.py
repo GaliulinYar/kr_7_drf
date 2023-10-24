26c08f5a0f24a4ed3808c5a4ea48c913acd2ed52
Yaroslav Galiulin <yaroslav66@list.ru>
2023-10-25 01:13:55 +0500

    Написание коментов

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from main.models import Habit
 from main.pagination import HabitPaginator
@@ -15,5 +15,18 @@ class HabitListAPIView(generics.ListAPIView):
     queryset = Habit.objects.filter(is_public=True)
 
 
+class MyHabitListAPIView(generics.ListAPIView):
+    """generic для вывода списка привычек текущего пользователя"""
+    serializer_class = HabitSerializer  # определяем сериализатор
+    pagination_class = HabitPaginator  # Пагинация для вывода по 5 привычек
+    permission_classes = [permissions.IsAuthenticated]  # подмешиваем авторизацию через токен API
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:  # проверка на аудентификацию пользователя
+            return Habit.objects.filter(owner=self.request.user)  # фильтруем привычки пользователя
+        return Habit.objects.none()
+
+
 class HabitCreateAPIView(generics.CreateAPIView):
-    """generic для вывода списка привычек других пользователей"""
+    """generic для создания списка привычек пользователей"""
+    serializer_class = HabitSerializer  # определяем сериализатор
