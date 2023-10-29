fe8f2bbd3d1530071e4e2358097f934571fa12dd
Yaroslav Galiulin <yaroslav66@list.ru>
2023-10-29 12:01:22 +0500

    функция отправки сообщения в чат

--- main/post_habit.py
+++ main/post_habit.py
@@ -3,11 +3,7 @@ import requests
 TOKEN = "6949650386:AAEDL0xtb5r0aM4Rd4hd-YfuQEHKpcxh9DE"
 
 
-def handle(message):
+def handle(message):  # функция отправки собщения в чат
     chat_id = "476890564"
-    # message = "Ща тебя привычками загружу"
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
     requests.get(url).json()  # Эта строка отсылает сообщение
-
-    # url = f"https://api.telegram.org/bot{TOKEN}/getUpdates" # узнать данные по чату
-    # print(requests.get(url).json())
